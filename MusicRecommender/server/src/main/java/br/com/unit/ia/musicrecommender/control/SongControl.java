b1ab52c36e6ce430277f9bc0ab8677098cc0315b
Jonas Tavares <jonjts@gmail.com>
2015-05-31 12:17:43 -0300

    Criado metodo que retorna uma musica aleatoria de um usuario.

--- MusicRecommender/server/src/main/java/br/com/unit/ia/musicrecommender/control/SongControl.java
+++ MusicRecommender/server/src/main/java/br/com/unit/ia/musicrecommender/control/SongControl.java
@@ -7,6 +7,9 @@ package br.com.unit.ia.musicrecommender.control;
 
 import br.com.unit.ia.musicrecommender.entity.Artist;
 import br.com.unit.ia.musicrecommender.entity.Song;
+import br.com.unit.ia.musicrecommender.entity.User;
+import br.com.unit.ia.musicrecommender.entity.UserArtist;
+import br.com.unit.ia.musicrecommender.persistence.ArtistPersistence;
 import br.com.unit.ia.musicrecommender.persistence.SongPersistence;
 import com.j256.ormlite.stmt.QueryBuilder;
 import java.sql.SQLException;
@@ -37,14 +40,14 @@ public class SongControl implements IControl<Song> {
     public Collection<Song> getAll() throws Exception {
         return getPersistence().listAll();
     }
-    
-    public Collection<Song> get(Artist artist) throws Exception{
+
+    public Collection<Song> get(Artist artist) throws Exception {
         QueryBuilder<Song, Long> queryBuilder = getPersistence().queryBuilder();
         queryBuilder.where().eq("artist_id", artist.getId());
         return queryBuilder.query();
     }
-    
-    public Collection<Song> getAll(long offset, long limit) throws Exception{
+
+    public Collection<Song> getAll(long offset, long limit) throws Exception {
         return getPersistence().queryBuilder().offset(offset).limit(limit).query();
     }
 
@@ -61,6 +64,24 @@ public class SongControl implements IControl<Song> {
     @Override
     public Song get(Number id) throws Exception {
         return getPersistence().findById(id.longValue());
+    }
+
+    public Song get(User user) throws SQLException {
+        QueryBuilder<Song, Long> queryBuilder = getPersistence().queryBuilder();
+        queryBuilder.where().ne("title", '-');
+        
+        UserArtistControl userArtistControl = new UserArtistControl();
+        QueryBuilder<UserArtist, Long> queryUserArtist = userArtistControl.getPersistence().queryBuilder();
+        queryUserArtist.where().eq("user_id", user.getId());
+        
+        ArtistControl artistControl = new ArtistControl();
+        QueryBuilder<Artist, Long> queryArtist = artistControl.getPersistence().queryBuilder();
+        
+        queryArtist.join(queryUserArtist);
+        
+        queryBuilder.join(queryArtist);
+        queryBuilder.orderByRaw("RAND()");
+        return queryBuilder.queryForFirst();
     }
 
 }
